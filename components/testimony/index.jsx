573f5bf23406b0b6695dd2d3f9134ba702b107df
Donny <doniwahyu14@gmail.com>
2023-03-06 12:32:19 +0700

    change testimony

--- components/testimony/index.jsx
+++ components/testimony/index.jsx
@@ -9,8 +9,8 @@ function Testimony() {
           <div className="card border-0 home-testimony-card">
             <div className="card-body">
               <p className="card-text home-testimony-text">
-                Jauh jauh dari bekasi ga sia sia dapet juga studio foto di
-                daerah boyolali Memuaskan, ramah. Mantep dah pokoknya🥰
+                Studionya bersih dan enak, pelayanan juga ramah dan enak diajak
+                ngobrol buat konsultasi tema foto 🥰
               </p>
               <div className="row home-testimony-user align-items-center">
                 <div className="col">
@@ -21,35 +21,8 @@ function Testimony() {
                   />
                 </div>
                 <div className="col-auto home-testimony-name">
-                  <h6 className="card-title home-testimony-name">Agung Acil</h6>
-                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
-                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
-                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
-                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
-                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col home-testimony-col">
-          <div className="card border-0 home-testimony-card">
-            <div className="card-body">
-              <p className="card-text home-testimony-text">
-                Pelayanan sangat memuaskan ramah dan humoris. Hasil fotonya
-                sangat memuaskan😍🤩
-              </p>
-              <div className="row home-testimony-user align-items-center">
-                <div className="col">
-                  <img
-                    src="/assets/testimony/testimony2.png"
-                    alt=""
-                    className="home-testimony-img rounded-circle"
-                  />
-                </div>
-                <div className="col-auto home-testimony-name">
                   <h6 className="card-title home-testimony-name">
-                    Irawati Azjuairiyah
+                    Agung Yamaguchi
                   </h6>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
@@ -65,18 +38,21 @@ function Testimony() {
           <div className="card border-0 home-testimony-card">
             <div className="card-body">
               <p className="card-text home-testimony-text">
-                Good Art & style photo studio
+                Hasil fotonya bagus dan mememuaskan banget. Bintang 5
+                pokoknya😍🤩
               </p>
               <div className="row home-testimony-user align-items-center">
                 <div className="col">
                   <img
-                    src="/assets/testimony/testimony3.png"
+                    src="/assets/testimony/testimony2.jpg"
                     alt=""
                     className="home-testimony-img rounded-circle"
                   />
                 </div>
                 <div className="col-auto home-testimony-name">
-                  <h6 className="card-title home-testimony-name">Hari Ongko</h6>
+                  <h6 className="card-title home-testimony-name">
+                    Kyoto Zumaki
+                  </h6>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
@@ -91,19 +67,47 @@ function Testimony() {
           <div className="card border-0 home-testimony-card">
             <div className="card-body">
               <p className="card-text home-testimony-text">
-                Mantabbbb hasil fotonya
+                Good photo studio. I would like to come back again.
               </p>
               <div className="row home-testimony-user align-items-center">
                 <div className="col">
                   <img
-                    src="/assets/testimony/testimony4.png"
+                    src="/assets/testimony/testimony3.jpg"
                     alt=""
                     className="home-testimony-img rounded-circle"
                   />
                 </div>
                 <div className="col-auto home-testimony-name">
                   <h6 className="card-title home-testimony-name">
-                    Odnafid k.z
+                    Hari Hari Bersamamu
+                  </h6>
+                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
+                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
+                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
+                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
+                  <i className="bi-star-fill" style={{ color: "yellow" }}></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col home-testimony-col">
+          <div className="card border-0 home-testimony-card">
+            <div className="card-body">
+              <p className="card-text home-testimony-text">
+                Good place to take photo
+              </p>
+              <div className="row home-testimony-user align-items-center">
+                <div className="col">
+                  <img
+                    src="/assets/testimony/testimony4.jpg"
+                    alt=""
+                    className="home-testimony-img rounded-circle"
+                  />
+                </div>
+                <div className="col-auto home-testimony-name">
+                  <h6 className="card-title home-testimony-name">
+                    Krisstina Ckrhakinov
                   </h6>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
                   <i className="bi-star-fill" style={{ color: "yellow" }}></i>
